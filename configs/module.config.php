f978c892eb80da78f74c70310a94979ca831f984
Evan Coury <me@evancoury.com>
2011-10-10 10:33:23 -0700

    Change 'methods' to 'parameters' in Di config

--- configs/module.config.php
+++ configs/module.config.php
@@ -6,13 +6,10 @@ return array(
                 'skeleton' => 'ZendSkeletonModule\Controller\SkeletonController',
             ),
             'Zend\View\PhpRenderer' => array(
-                'methods' => array(
-                    'setResolver' => array(
-                        'resolver' => 'Zend\View\TemplatePathStack',
-                        'options'  => array(
-                            'script_paths' => array(
-                                'skeleton' => __DIR__ . '/../views',
-                            ),
+                'parameters' => array(
+                    'options'  => array(
+                        'script_paths' => array(
+                            'skeleton' => __DIR__ . '/../views',
                         ),
                     ),
                 ),
