31d9329c36c59d2bc48b14c7af88e65ea058b438
tr <tr@>
2012-07-29 05:30:06 +0100

    removing directory_separator constant

--- Module.php
+++ Module.php
@@ -23,7 +23,7 @@ class Module implements AutoloaderProviderInterface
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
 		    // if we're in a namespace deeper than one level we need to fix the \ in the path
-                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', DIRECTORY_SEPARATOR, __NAMESPACE__),
+                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/' , __NAMESPACE__),
                 ),
             ),
         );
