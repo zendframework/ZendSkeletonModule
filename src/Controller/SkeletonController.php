f798d4582873b2f4b5c2f84581b8f4638bd33fdf
Matthew Weier O'Phinney <matthew@zend.com>
2016-06-28 06:53:22 -0500

    Updated to latest recommendations
    This patch does the following:
    
    - It removes the generic fallthrough route that matches on the
      controller and action, as this is not recommended; as such, it also
      removes the "foo" action and view, as they are no longer routable.
    - It removes all autoloading artifacts, and details how to setup
      autoloading in composer.
    - It switches to a PSR-4 structure.
    - It updates to short-array notation.
    - Configuration is updated to use `::class` notation.

--- src/Controller/SkeletonController.php
+++ src/Controller/SkeletonController.php
@@ -1,9 +1,7 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
  * @link      http://github.com/zendframework/ZendSkeletonModule for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
@@ -15,13 +13,6 @@ class SkeletonController extends AbstractActionController
 {
     public function indexAction()
     {
-        return array();
-    }
-
-    public function fooAction()
-    {
-        // This shows the :controller and :action parameters in default route
-        // are working when you browse to /module-specific-root/skeleton/foo
-        return array();
+        return [];
     }
 }
